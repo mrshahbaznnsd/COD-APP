ee3198919b265f34fce072608a8bf131e8222435
mrshahbaznnsd <mrshahbaznnsd@gmail.com>
2025-01-09 20:04:07 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'], // Add this if you want to externalize react-router-dom
+    },
+  },
 });
